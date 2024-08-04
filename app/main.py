5d0e1c7558e6bc05f69e44983716d0e39783f379
Sara <gunawardhanapulsara@gmail.com>
2024-08-04 13:47:42 +0100

    Building the api

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/")
+def root(): 
+     return {'Hello':'World'}
+ 
+@app.post("/description")
+def get_description(description:str): 
+    print(description)
